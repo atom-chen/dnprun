39c31af54feaf65a05be8be52e95930fb222faeb
qihaizhifeng <954038069@qq.com>
2016-07-06 12:35:07 +0800

    dnprun

--- frameworks/runtime-src/Classes/Const.h
+++ frameworks/runtime-src/Classes/Const.h
@@ -9,7 +9,7 @@
 #ifndef Template_Const_h
 #define Template_Const_h
 
-#define UUID_IOS_KEYCHAIN "5G2TD45G3P.com.donopo.overlap"
+#define UUID_IOS_KEYCHAIN "5G2TD45G3P.com.donopo.dnprun"
 
 #define XXTEA_KEY "d@n0po"
 #define XXTEA_SIGN "donopo"
